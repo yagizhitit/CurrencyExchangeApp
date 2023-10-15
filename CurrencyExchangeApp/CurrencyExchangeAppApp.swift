5049abee1d0a1f2e8760c10fc89a3dd129c134aa
Yagiz Hitit <yagizhitit@gmail.com>
2023-10-15 22:33:05 +0100

    Initial Commit

---
+++ CurrencyExchangeApp/CurrencyExchangeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyExchangeAppApp.swift
+//  CurrencyExchangeApp
+//
+//  Created by Yağız Hitit on 15.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyExchangeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
