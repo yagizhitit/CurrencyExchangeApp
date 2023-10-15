d536889fda41c636fc07b98ede6d34eed33efa64
Yagiz Hitit <yagizhitit@gmail.com>
2023-10-15 22:36:57 +0100

    Initial Commit

--- CurrencyExchangeApp/ContentView.swift
+++ CurrencyExchangeApp/ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Button("Button title") {
+                print("Button tapped!")
+            }
         }
         .padding()
     }
