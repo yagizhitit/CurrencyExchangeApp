c9c9484b9de149c5e77bccfac40638c628b96c1a
Yagiz Hitit <yagizhitit@gmail.com>
2023-10-19 19:19:04 +0100

    Reach the API and get the currency rate from API

--- CurrencyExchangeApp/ContentView.swift
+++ CurrencyExchangeApp/ContentView.swift
@@ -9,18 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var food = ""
-    var foodArray = ["Milk", "Apples", "Sugar", "Eggs", "Oranges", "Potatoes", "Corn", "Bread"]
     @StateObject private var countriesVM = CurrencyViewModel()
+    
     @State private var selectedCountry1 = ""
     @State private var selectedCountry2 = ""
     @State private var presentPicker1 = false
     @State private var presentPicker2 = false
-    @State private var tag1: Int = 1
-    @State private var tag2: Int = 2
     
     @State private var numberInput1 = ""
     @State private var numberInput2 = ""
+    
+    @State private var tag: Int = 0
+    
+    func currencyCodeFromCountryString(_ countryString: String) -> String? {
+        let components = countryString.components(separatedBy: "(")
+        guard components.count > 1 else { return nil }
+        let code = components[1].trimmingCharacters(in: CharacterSet(charactersIn: " )"))
+        return code
+    }
+
+    func convertCurrency() {
+        guard let baseCode = countriesVM.getCodeForCountry(selectedCountry1),
+              let targetCode = countriesVM.getCodeForCountry(selectedCountry2) else {
+            return
+        }
+        
+        print("Base Currency: \(baseCode)")
+        print("Target Currency: \(targetCode)")
+        print("Amount: \(numberInput1)")
+        
+        let urlString = "https://api.apilayer.com/exchangerates_data/latest?symbols=\(targetCode)&base=\(baseCode)"
+        print("Generated URL: \(urlString)")
+        
+        if let rate = countriesVM.getCurrentRate(for: targetCode),
+           let inputNumber = Double(numberInput1) {
+            numberInput2 = String(format: "%.2f", inputNumber * rate)
+        } else {
+            countriesVM.fetchSpecificExchangeRate(baseCurrency: baseCode, targetCurrency: targetCode)
+        }
+    }
+
+
     
     var body: some View {
         ZStack {
@@ -41,28 +70,9 @@ struct ContentView: View {
                             Divider()
                                 .frame(width: 350)
                             
-                            CustomPickerTextView(presentPicker: $presentPicker1, fieldString: $selectedCountry1, placeholder: "Select a currency", tag: $tag1, selectedTag: 1)
+                            CustomPickerTextView(presentPicker: $presentPicker1, fieldString: $selectedCountry1, placeholder: "Select a currency", tag: $tag, selectedTag: 1)
+
                         }
-                        
-                        HStack {
-                            VStack {
-                                Rectangle()
-                                    .fill(Color("firstColor"))
-                                    .frame(width: 280,height: 1)
-                                
-                            }
-                            
-                            Button(action: {
-                                fetchAPIData()
-                            }) {
-                                Image(systemName: "arrow.up.arrow.down.circle.fill")
-                                    .font(.largeTitle)
-                                    .foregroundColor(Color("firstColor"))
-                                    .padding(.leading, 10)
-                            }
-                        }
-                        .padding(.top, 80)
-                        .padding(.bottom, 80)
                         
                         VStack {
                             TextField("", text: $numberInput2)
@@ -74,14 +84,28 @@ struct ContentView: View {
                             Divider()
                                 .frame(width: 350)
                             
-                            CustomPickerTextView(presentPicker: $presentPicker2, fieldString: $selectedCountry2, placeholder: "Select a currency", tag: $tag2, selectedTag: 2)
+                            CustomPickerTextView(presentPicker: $presentPicker2, fieldString: $selectedCountry2, placeholder: "Select a currency", tag: $tag, selectedTag: 2)
+
                         }
+                        
                         Spacer()
+                        
+                        Button(action: {
+                            convertCurrency()
+                        }) {
+                            Text("Calculate")
+                                .font(.title2)
+                                .padding()
+                                .background(Color.blue)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                        }
+                        .padding(.bottom)
+                        
                     }
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .frame(width: 250)
                     .navigationBarTitle("Currency Exchange", displayMode: .inline)
-
                 }
                 .background(Color("backgroundColor"))
             }
@@ -94,39 +118,8 @@ struct ContentView: View {
     }
 }
 
-func fetchAPIData() {
-    var semaphore = DispatchSemaphore (value: 0)
-
-    // Örnek URL, bu URL'yi doğru değerlerle değiştirmeniz gerekebilir.
-    let urlString = "https://api.apilayer.com/exchangerates_data/latest?symbols=EUR&base=USD"
-
-    if let validURL = URL(string: urlString) {
-        var request = URLRequest(url: validURL, timeoutInterval: Double.infinity)
-        request.httpMethod = "GET"
-        request.addValue("3rYeJF1etcelsHMG9BKriNI5VX0yW1s7", forHTTPHeaderField: "apikey")
-        
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data else {
-                print(String(describing: error))
-                return
-            }
-            print(String(data: data, encoding: .utf8)!)
-            semaphore.signal()
-        }
-
-        task.resume()
-        semaphore.wait()
-    } else {
-        print("Geçersiz URL")
-    }
-
-}
-
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
-
-
--- CurrencyExchangeApp/CurrencyFiles/CurrencyViewModel.swift
+++ CurrencyExchangeApp/CurrencyFiles/CurrencyViewModel.swift
@@ -12,22 +12,39 @@ class CurrencyViewModel: NSObject, ObservableObject {
     @Published var currencies = [Currency]()
     @Published var exchangeRates: [String: Double] = [:]
     
-    var countryNamesArray: [String] {
-        currencies.map { "\($0.flag) \($0.name) (\($0.currency.code ?? "Unknown"))" }.sorted()
-    }
+    var onExchangeRatesUpdated: (() -> Void)?
     
+    var countryNamesArray: [String] {
+        let uniqueCurrencies = Array(Set(currencies.map { "\($0.flag) \($0.name) (\($0.currency.code ?? "Unknown"))" }))
+        return uniqueCurrencies.sorted()
+    }
+
     override init() {
         currencies = Store.retrieveCountries()
         super.init()
-        fetchExchangeRates()
+        fetchInitialExchangeRates()
     }
     
-    func fetchExchangeRates() {
-        // Opsiyonel olmayan değerlere sahip currency kodlarına filtreleme işlemi
+    func fetchInitialExchangeRates() {
         let nonOptionalCurrencyCodes = self.currencies.compactMap { $0.currency.code }
-        
         let urlString = "https://api.apilayer.com/exchangerates_data/latest?symbols=\(nonOptionalCurrencyCodes.joined(separator: ","))&base=USD"
-        
+        fetchExchangeRates(from: urlString)
+    }
+
+    func getExchangeRate(from baseCurrency: String, to targetCurrency: String) -> Double? {
+        return exchangeRates["\(baseCurrency)_\(targetCurrency)"]
+    }
+    
+    func fetchSpecificExchangeRate(baseCurrency: String, targetCurrency: String) {
+        let urlString = "https://api.apilayer.com/exchangerates_data/latest?symbols=\(targetCurrency)&base=\(baseCurrency)"
+        fetchExchangeRates(from: urlString)
+    }
+    
+    func getCurrentRate(for currencyCode: String) -> Double? {
+        return exchangeRates[currencyCode]
+    }
+    
+    private func fetchExchangeRates(from urlString: String) {
         guard let url = URL(string: urlString) else {
             print("Invalid URL.")
             return
@@ -35,7 +52,7 @@ class CurrencyViewModel: NSObject, ObservableObject {
         
         var request = URLRequest(url: url, timeoutInterval: Double.infinity)
         request.httpMethod = "GET"
-        request.addValue("3rYeJF1etcelsHMG9BKriNI5VX0yW1s7", forHTTPHeaderField: "apikey")
+        request.addValue("3rYeJF1etcelsHMG9BKriNI5VX0yW1s7", forHTTPHeaderField: "apikey")  // Not: API anahtarınızı bu şekilde saklamamanızı öneririm!
         
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             guard let data = data else {
@@ -47,6 +64,7 @@ class CurrencyViewModel: NSObject, ObservableObject {
                 let decodedResponse = try JSONDecoder().decode(ExchangeRateResponse.self, from: data)
                 DispatchQueue.main.async {
                     self.exchangeRates = decodedResponse.rates
+                    self.onExchangeRatesUpdated?()
                 }
             } catch {
                 print("Error decoding JSON: \(error.localizedDescription)")
@@ -54,9 +72,16 @@ class CurrencyViewModel: NSObject, ObservableObject {
         }
         task.resume()
     }
-
+    
+    func getCodeForCountry(_ countryDetail: String) -> String? {
+        let components = countryDetail.components(separatedBy: "(")
+        guard components.count > 1 else { return nil }
+        let code = components[1].trimmingCharacters(in: CharacterSet(charactersIn: " )"))
+        return code
+    }
 }
 
 struct ExchangeRateResponse: Codable {
     let rates: [String: Double]
 }
+
